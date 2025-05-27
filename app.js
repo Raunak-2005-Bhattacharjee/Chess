626ec973b0f86ba967286ac84bd252262a1af2d0
Raunak-2005-Bhattacharjee <raunakbhattcharjee@gmail.com>
2025-05-27 18:27:48 +0530

    Update server listen address to production URL

--- app.js
+++ app.js
@@ -79,6 +79,6 @@ io.on('connection', (uniquesocket) => {
     })
 })
 
-server.listen(3000, () => {
+server.listen("https://chess-teal-xi.vercel.app/", () => {
     console.log('Server is running on http://localhost:3000');
 });
