26e6219b3514b0473715b0fa5fdd2f1c379c9259
Raunak-2005-Bhattacharjee <raunakbhattcharjee@gmail.com>
2025-06-07 20:07:22 +0530

    Made some changes for deployment

--- app.js
+++ app.js
@@ -79,6 +79,6 @@ io.on('connection', (uniquesocket) => {
     })
 })
 
-server.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+server.listen(3001, () => {
+    console.log('Server is running on http://localhost:3001');
 });
